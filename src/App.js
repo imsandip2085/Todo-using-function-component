9f8e7f7997b7f84b70c77819450e79b13ce43529
imsandip10 <sandybro9919@gmail.com>
2020-06-16 19:24:19 +0530

    completed

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import FormSubmit from './form';
 
 function App() {
   return (
     <div className="App">
-     
+     <FormSubmit />
     </div>
   );
 }
--- src/form.jsx
+++ src/form.jsx
@@ -1,1 +1,80 @@
- 
+import React, { useState } from 'react';
+import { Card,Form,Row,Col, Button } from 'react-bootstrap';
+import './App.css';
+
+function FormSubmit () {
+   
+    const [title, setTitle] = useState("")
+    const [todoItem , setTodoItem] = useState([]);
+
+ const handleSubmit = e => {
+     e.preventDefault();
+     if(!title == ""){
+       let todo= {
+         todo:title,
+         completed:false
+       }
+       setTodoItem([...todoItem, todo])
+     }
+     setTitle("")
+
+
+  };
+   
+  const handleDeleteListItem = (id) =>{
+   let todoItems = [...todoItem];
+     todoItems.splice(id, 1);
+   setTodoItem(todoItems);
+  }
+
+  const handleChange = event => {
+    setTitle(event.target.value)
+  };
+
+const handleChecked = (id, name, checkedValues) =>{
+
+    let todoItems = todoItem.map(val => {
+      if (val.todo === name) {
+        val.completed = !val.completed ;
+      }
+      return val
+    });
+    setTodoItem(todoItems)
+} 
+
+    return(
+        <div className="todoForm">
+             <h1>Todo App</h1>
+             <form onSubmit={handleSubmit}>
+                <input type="text" 
+                   placeholder="Todo..." 
+                   value={title} 
+                   onChange={(event)=>handleChange(event)}>
+                </input>
+             </form>
+              
+               {
+                  todoItem.map((val, key) =>{
+                   return (
+                     <div>
+                         <input
+                            checked={val.checked}
+                            type="checkbox"
+                            id={key}
+                            value={val.todo}
+                            name="title"
+                            onClick={() => handleChecked(key, val.todo, val.completed)}
+                          />   
+                             <p style={{ textDecoration: val.completed == true ? "line-through" : "" }}>{val.todo}</p>
+                             <Button onClick={() => handleDeleteListItem(key)}>X</Button>
+                             </div>
+                  )
+                  }) 
+               }
+              
+               
+        </div>
+    )
+}
+
+export default FormSubmit;
