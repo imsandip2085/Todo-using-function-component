dd822f0cd72857da996fdce1ad2809513e5f419f
imsandip10 <sandybro9919@gmail.com>
2020-06-22 20:07:53 +0530

    incompleted

--- src/form.jsx
+++ src/form.jsx
@@ -2,17 +2,13 @@ import React, { useState } from 'react';
 import { Card,Form,Row,Col, Button } from 'react-bootstrap';
 import './App.css';
 
-function FormSubmit () {
-   
-    const [title, setTitle] = useState("")
-    const [todoItem , setTodoItem] = useState([]);
-    const [filterTodoItemList , setFilterTodoItemList] = useState([]);
-    const [storageItem , setStorageItem] = useState(JSON.parse(localStorage.getItem('user')) != 0 ?
-    [JSON.parse(localStorage.getItem('user'))] : [0]);
-    const [storageItem2 , setStorageItem2] = useState(JSON.parse(localStorage.getItem('user')) != 0 ?
-    [JSON.parse(localStorage.getItem('user'))] : [0]);
+function FormSubmit () { 
+  const [title, setTitle] = useState("")
+  const [todoItem , setTodoItem] = useState([]);
+  const [storeTodoItem , setStoreTodoItem] = useState(JSON.parse(localStorage.getItem('user')) != 0 ? JSON.parse(localStorage.getItem('user')) : [0]);
+  const [buttonStatus, setButtonStatus] = useState('')
  
- console.log(storageItem,"sssssssssssssssssssssssssssssssssssssssss");
+
  const handleSubmit = e => {
      e.preventDefault();
      if(!title == ""){
@@ -22,9 +18,6 @@ function FormSubmit () {
          completed:false
        }
        setTodoItem([...todoItem, todo])
-       setFilterTodoItemList([...filterTodoItemList, todo])
-       setStorageItem([...storageItem, todo])
-       setStorageItem([...storageItem2, todo])
      }
      setTitle("")
   };
@@ -33,111 +26,108 @@ function FormSubmit () {
    let todoItems = [...todoItem];
    todoItems.splice(id, 1);
    setTodoItem(todoItems);
-   let filterTodoItemLists = [...filterTodoItemList];
-   filterTodoItemList.splice(id, 1);
-   setFilterTodoItemList(filterTodoItemList)
-
-   let storageItems = [...storageItem];
-   storageItems.splice(id, 1);
-   setFilterTodoItemList(storageItems)
-
-   let storageItems2 = [...storageItem2];
-   storageItems2.splice(id, 1);
-   setFilterTodoItemList(storageItems2)
 
   }
-
   const handleChange = event => {
     setTitle(event.target.value)
   };
 
 const handleChecked = (id, name, checkedValues) =>{
-    let todoItems = filterTodoItemList.map(val => {
+    let todoItems = todoItem.map(val => {
       if (val.id === id) {
         val.completed = !val.completed ;
       }
       return val
     });
     localStorage.setItem('user', JSON.stringify(todoItems));
-    setStorageItem(todoItems)
     setTodoItem(todoItems)
-
-    let todoItems2 = storageItem.map(val => {
-      if (val.id === id) {
-        val.completed = !val.completed ;
-      }
-      return val
-    });
-
-    setFilterTodoItemList(todoItems2)
-    setStorageItem2(todoItems2)
+    // setStoreTodoItem(JSON.parse(localStorage.getItem('user')))
 } 
 
+
+const handleSubmitStore = e => {
+  e.preventDefault();
+  if(!title == ""){
+    let todo= {
+      id : Math.random(),
+      todo:title,
+      completed:false
+    }
+     setStoreTodoItem([...storeTodoItem, todo])
+  }
+  setTitle("")
+};
+
+const handleDeleteListItemStore = (id) =>{
+let storeTodoItems = [...storeTodoItem];
+storeTodoItems.splice(id, 1);
+setStoreTodoItem(storeTodoItems);
+
+}
+const handleChangeStore = event => {
+ setTitle(event.target.value)
+};
+
+const handleCheckedStore = (id, name, checkedValues) =>{
+  let storeTodoItems = storeTodoItem.map(val => {
+    if (val.id === id) {
+      val.completed = !val.completed ;
+    }
+    return val
+  });
+  setStoreTodoItem(storeTodoItems)
+  // setStoreTodoItem(JSON.parse(localStorage.getItem('user')))
+} 
+
+
+
+
+
+
 const handleClickCompleted = () =>{
-var completedListItems = todoItem.filter(function(val){
-  return val.completed == true;
-})
-setStorageItem(completedListItems)
-setFilterTodoItemList(completedListItems);
-
-var completedListItems2 = storageItem.filter(function(val){
-  return val.completed == true;
-})
-setStorageItem(completedListItems2)
-setStorageItem2(completedListItems2) 
-
+  setButtonStatus("completed")
 }
 
 const handleClickActiveList = () =>{
-  
-  var activeListItems = todoItem.filter(function(val){
-    return val.completed == false;
-  })
-  setFilterTodoItemList(activeListItems);
-  setStorageItem(activeListItems) 
+  setButtonStatus("activeList")
 
-  var activeListItems2 = storageItem.filter(function(val){
-    return val.completed == false;
-  })
-  setStorageItem(activeListItems2)
-  setStorageItem2(activeListItems2) 
-  }
+}
   
 const handleClickAll = () =>{ 
-  setStorageItem2(storageItem) 
-  setFilterTodoItemList(todoItem);
-}
+  setButtonStatus("all")
 
+}
 
 return(
         <div className="todoForm">
              <h1>Todo App</h1>
-             <form onSubmit={handleSubmit}>
-                <input type="text" 
-                   placeholder="Todo..." 
-                   value={title} 
-                   onChange={(event)=>handleChange(event)}>
-                </input>
-             </form>            
+             {
+                (localStorage.getItem('user') === null) ? 
+                (
+                  <form onSubmit={handleSubmit}>
+                  <input type="text" 
+                     placeholder="Todo..." 
+                     value={title} 
+                     onChange={(event)=>handleChange(event)}>
+                  </input>
+               </form>  
+                ):
+                (
+                  <form onSubmit={handleSubmitStore}>
+                  <input type="text" 
+                     placeholder="Todo..." 
+                     value={title} 
+                     onChange={(event)=>handleChangeStore(event)}>
+                  </input>
+               </form>
+                )
+             }
+          
                {
-                 (JSON.parse(localStorage.getItem('user')) != 0 ?
-                 storageItem.map((val, key) =>{
-                   return (
-                    <div>
-                      <input
-                       checked={val.checked}
-                       type="checkbox"
-                       id={val.id}
-                       value={val.todo}
-                       name="title"
-                       onClick={() => handleChecked(val.id, val.todo, val.completed)}
-                      />   
-                      <p style={{ textDecoration: val.completed == true ? "line-through" : "" }}>{val.todo}</p>
-                      <Button onClick={() => handleDeleteListItem(key)}>X</Button>
-                    </div>
-                  )
-                  })  :
-               filterTodoItemList.map((val, key) =>{
+                 (localStorage.getItem('user') === null) ?
+                ( 
+                  buttonStatus =="all"  ? 
+                todoItem.map((val, key) =>{
                    return (
                     <div>
                       <input
@@ -152,8 +142,151 @@ return(
                        <Button onClick={() => handleDeleteListItem(key)}>X</Button> 
                     </div>
                   )
+                  })  : 
+                    (buttonStatus =="activeList" ?
+                      todoItem.map((val, key) =>{ 
+                        return (
+                         val.completed === false ?
+                         (
+                         <div>
+                           <input
+                            checked={val.checked}
+                            type="checkbox"
+                            id={val.id}
+                            value={val.todo}
+                            name="title"
+                            onClick={() => handleChecked(val.id, val.todo, val.completed)}
+                           />   
+                           <p style={{ textDecoration: val.completed == true ? "line-through" : "" }}>{val.todo}</p>
+                           <Button onClick={() => handleDeleteListItem(key)}>X</Button>
+                         </div>
+                         ): null
+                        )
+                      })
+                      :
+                      (buttonStatus =="completed"  ? 
+                      todoItem.map((val, key) =>{
+                       return (
+                        val.completed === true ?
+                        (
+                       <div>
+                         <input
+                           checked={val.checked}
+                           type="checkbox"
+                           id={val.id}
+                            value={val.todo}
+                           name="title"
+                          onClick={() => handleChecked(val.id, val.todo, val.completed)}
+                          />   
+                         <p style={{ textDecoration: val.completed == true ? "line-through" : "" }}>{val.todo}</p>
+                         <Button onClick={() => handleDeleteListItem(key)}>X</Button> 
+                    </div>
+                    ) : null
+                  )
                   }) 
-                 )
+                     :
+                        todoItem.map((val, key) =>{
+                          return (
+                           <div>
+                             <input
+                              checked={val.checked}
+                              type="checkbox"
+                              id={val.id}
+                              value={val.todo}
+                              name="title"
+                              onClick={() => handleChecked(val.id, val.todo, val.completed)}
+                             />   
+                             <p style={{ textDecoration: val.completed == true ? "line-through" : "" }}>{val.todo}</p>
+                             <Button onClick={() => handleDeleteListItem(key)}>X</Button>
+                           </div>
+                         )
+                        })
+                       )
+                  )
+                  )
+                   : (
+                    // <div>sdf</div>
+                    (buttonStatus =="all"  ? 
+                    storeTodoItem.map((val, key) =>{
+                       return (
+                        <div>
+                          <input
+                           checked={val.checked}
+                           type="checkbox"
+                           id={val.id}
+                           value={val.todo}
+                           name="title"
+                           onClick={() => handleCheckedStore(val.id, val.todo, val.completed)}
+                          />   
+                          <p style={{ textDecoration: val.completed == true ? "line-through" : "" }}>{val.todo}</p>
+                           <Button onClick={() => handleDeleteListItemStore(key)}>X</Button> 
+                        </div>
+                      )
+                      }) 
+                       : 
+                        (buttonStatus =="activeList" ?
+                        storeTodoItem.map((val, key) =>{ 
+                            return (
+                             val.completed === false ?
+                             (
+                             <div>
+                               <input
+                                checked={val.checked}
+                                type="checkbox"
+                                id={val.id}
+                                value={val.todo}
+                                name="title"
+                                onClick={() => handleCheckedStore(val.id, val.todo, val.completed)}
+                               />   
+                               <p style={{ textDecoration: val.completed == true ? "line-through" : "" }}>{val.todo}</p>
+                               <Button onClick={() => handleDeleteListItemStore(key)}>X</Button>
+                             </div>
+                             ): null
+                            )
+                          })
+                          :
+                          (buttonStatus =="completed"  ? 
+                          storeTodoItem.map((val, key) =>{
+                           return (
+                            val.completed === true ?
+                            (
+                           <div>
+                             <input
+                               checked={val.checked}
+                               type="checkbox"
+                               id={val.id}
+                                value={val.todo}
+                               name="title"
+                              onClick={() => handleCheckedStore(val.id, val.todo, val.completed)}
+                              />   
+                             <p style={{ textDecoration: val.completed == true ? "line-through" : "" }}>{val.todo}</p>
+                             <Button onClick={() => handleDeleteListItemStore(key)}>X</Button> 
+                        </div>
+                        ) : null
+                      )
+                      }) 
+                         :
+                         storeTodoItem.map((val, key) =>{
+                              return (
+                               <div>
+                                 <input
+                                  checked={val.checked}
+                                  type="checkbox"
+                                  id={val.id}
+                                  value={val.todo}
+                                  name="title"
+                                  onClick={() => handleCheckedStore(val.id)}
+                                 />   
+                                 <p style={{ textDecoration: val.completed == true ? "line-through" : "" }}>{val.todo}</p>
+                                 <Button onClick={() => handleDeleteListItemStore(key)}>X</Button>
+                               </div>
+                             )
+                            })
+                           )
+                      )
+                      )
+                  )
+                  
                } 
                <Button type="submit" variant="outline-danger" onClick={() =>handleClickAll()}>All</Button>{''}
                <Button type="submit" variant="outline-danger" onClick={() =>handleClickActiveList()}>ActiveList</Button>{''}
@@ -163,3 +296,4 @@ return(
 }
 
 export default FormSubmit;
+
