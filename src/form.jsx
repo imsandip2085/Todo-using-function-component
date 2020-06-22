4a425e66084b2becead78a7cb95df8025f12b66d
imsandip10 <sandybro9919@gmail.com>
2020-06-22 15:14:41 +0530

    incomplete localStorage

--- src/form.jsx
+++ src/form.jsx
@@ -7,8 +7,12 @@ function FormSubmit () {
     const [title, setTitle] = useState("")
     const [todoItem , setTodoItem] = useState([]);
     const [filterTodoItemList , setFilterTodoItemList] = useState([]);
-
-    localStorage.setItem('user', JSON.stringify(filterTodoItemList));
+    const [storageItem , setStorageItem] = useState(JSON.parse(localStorage.getItem('user')) != 0 ?
+    [JSON.parse(localStorage.getItem('user'))] : [0]);
+    const [storageItem2 , setStorageItem2] = useState(JSON.parse(localStorage.getItem('user')) != 0 ?
+    [JSON.parse(localStorage.getItem('user'))] : [0]);
+ 
+ console.log(storageItem,"sssssssssssssssssssssssssssssssssssssssss");
  const handleSubmit = e => {
      e.preventDefault();
      if(!title == ""){
@@ -19,17 +23,28 @@ function FormSubmit () {
        }
        setTodoItem([...todoItem, todo])
        setFilterTodoItemList([...filterTodoItemList, todo])
+       setStorageItem([...storageItem, todo])
+       setStorageItem([...storageItem2, todo])
      }
      setTitle("")
   };
    
   const handleDeleteListItem = (id) =>{
    let todoItems = [...todoItem];
-     todoItems.splice(id, 1);
+   todoItems.splice(id, 1);
    setTodoItem(todoItems);
    let filterTodoItemLists = [...filterTodoItemList];
    filterTodoItemList.splice(id, 1);
    setFilterTodoItemList(filterTodoItemList)
+
+   let storageItems = [...storageItem];
+   storageItems.splice(id, 1);
+   setFilterTodoItemList(storageItems)
+
+   let storageItems2 = [...storageItem2];
+   storageItems2.splice(id, 1);
+   setFilterTodoItemList(storageItems2)
+
   }
 
   const handleChange = event => {
@@ -43,35 +58,58 @@ const handleChecked = (id, name, checkedValues) =>{
       }
       return val
     });
-    localStorage.setItem('todoItemStorage', JSON.stringify(todoItems));
-
+    localStorage.setItem('user', JSON.stringify(todoItems));
+    setStorageItem(todoItems)
     setTodoItem(todoItems)
-    setFilterTodoItemList(todoItems)
+
+    let todoItems2 = storageItem.map(val => {
+      if (val.id === id) {
+        val.completed = !val.completed ;
+      }
+      return val
+    });
+
+    setFilterTodoItemList(todoItems2)
+    setStorageItem2(todoItems2)
 } 
 
 const handleClickCompleted = () =>{
-var completedListItems = JSON.parse(localStorage.getItem('todoItemStorage')).filter(function(val){
+var completedListItems = todoItem.filter(function(val){
   return val.completed == true;
 })
-
+setStorageItem(completedListItems)
 setFilterTodoItemList(completedListItems);
+
+var completedListItems2 = storageItem.filter(function(val){
+  return val.completed == true;
+})
+setStorageItem(completedListItems2)
+setStorageItem2(completedListItems2) 
+
 }
 
 const handleClickActiveList = () =>{
   
-  var activeListItems = JSON.parse(localStorage.getItem('todoItemStorage')).filter(function(val){
+  var activeListItems = todoItem.filter(function(val){
     return val.completed == false;
   })
   setFilterTodoItemList(activeListItems);
+  setStorageItem(activeListItems) 
+
+  var activeListItems2 = storageItem.filter(function(val){
+    return val.completed == false;
+  })
+  setStorageItem(activeListItems2)
+  setStorageItem2(activeListItems2) 
   }
   
-const handleClickAll = () =>{
-   
-  setFilterTodoItemList(JSON.parse(localStorage.getItem('todoItemStorage')));
+const handleClickAll = () =>{ 
+  setStorageItem2(storageItem) 
+  setFilterTodoItemList(todoItem);
 }
 
-  // console.log( JSON.parse(localStorage.getItem('user')) == 0);
-    return(
+
+return(
         <div className="todoForm">
              <h1>Todo App</h1>
              <form onSubmit={handleSubmit}>
@@ -81,10 +119,25 @@ const handleClickAll = () =>{
                    onChange={(event)=>handleChange(event)}>
                 </input>
              </form>            
-               { 
-                (JSON.parse(localStorage.getItem('todoItemStorage')) == 0 ? 
-                todoItem.map((val, key) =>{
-                  console.log("AAAAAAAAAAAAA")
+               {
+                 (JSON.parse(localStorage.getItem('user')) != 0 ?
+                 storageItem.map((val, key) =>{
+                   return (
+                    <div>
+                      <input
+                       checked={val.checked}
+                       type="checkbox"
+                       id={val.id}
+                       value={val.todo}
+                       name="title"
+                       onClick={() => handleChecked(val.id, val.todo, val.completed)}
+                      />   
+                      <p style={{ textDecoration: val.completed == true ? "line-through" : "" }}>{val.todo}</p>
+                      <Button onClick={() => handleDeleteListItem(key)}>X</Button>
+                    </div>
+                  )
+                  })  :
+               filterTodoItemList.map((val, key) =>{
                    return (
                     <div>
                       <input
@@ -99,47 +152,8 @@ const handleClickAll = () =>{
                        <Button onClick={() => handleDeleteListItem(key)}>X</Button> 
                     </div>
                   )
-                  })  
-                  : 
-                   (JSON.parse(localStorage.getItem('user')) !== 0 ?
-                  JSON.parse(localStorage.getItem('user')).map((val, key) =>{
-                    console.log("eeeeeeee")
-                   return (
-                    <div>
-                      <input
-                       checked={val.checked}
-                       type="checkbox"
-                       id={val.id}
-                       value={val.todo}
-                       name="title"
-                       onClick={() => handleChecked(val.id, val.todo, val.completed)}
-                      />   
-                      <p style={{ textDecoration: val.completed == true ? "line-through" : "" }}>{val.todo}</p>
-                      <Button onClick={() => handleDeleteListItem(key)}>X</Button>
-                    </div>
-                  )
                   }) 
-                  : 
-                  (JSON.parse(localStorage.getItem('user')) === 0 ?
-                  console.log("SSSSSSSSSS"):
-                  JSON.parse(localStorage.getItem('todoItemStorage')).map((val, key) =>{
-                    console.log("BBBBBBBBBBBBB")
-                   return (
-                    <div>
-                      <input
-                       checked={val.checked}
-                       type="checkbox"
-                       id={val.id}
-                       value={val.todo}
-                       name="title"
-                       onClick={() => handleChecked(val.id, val.todo, val.completed)}
-                      />   
-                      <p style={{ textDecoration: val.completed == true ? "line-through" : "" }}>{val.todo}</p>
-                      <Button onClick={() => handleDeleteListItem(key)}>X</Button>
-                    </div>
-                  )
-                  }) 
-                )))
+                 )
                } 
                <Button type="submit" variant="outline-danger" onClick={() =>handleClickAll()}>All</Button>{''}
                <Button type="submit" variant="outline-danger" onClick={() =>handleClickActiveList()}>ActiveList</Button>{''}
